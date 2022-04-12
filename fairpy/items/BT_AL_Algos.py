43a6c481c3e2d80c150421862af90a884be4802e
TalAbed <talabed46@gmail.com>
2022-04-12 15:13:00 +0300

    with mine code

--- fairpy/items/BT_AL_Algos.py
+++ fairpy/items/BT_AL_Algos.py
@@ -1,17 +1,17 @@
-# def BT (playerA, playerB):
-#     """
-#     >>> BT(0,0)
-#     1
-#     """
-#     return 0
+def BT (playerA, playerB):
+    """
+    >>> BT(0,0)
+    1
+    """
+    return 0
 
-# def AL (playerA, playerB):
-#     """
-#     >>> AL(0,0)
-#     1
-#     """
-#     return 7
+def AL (playerA, playerB):
+    """
+    >>> AL(0,0)
+    1
+    """
+    return 7
 
-# if __name__ == "__main__":
-#     import doctest
-#     print(doctest.testmod())
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
